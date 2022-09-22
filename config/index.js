f59d18a7e6d737df6c8ca61770fb93cd255e09ae
JoEkko13 <114058397+JoEkko13@users.noreply.github.com>
2022-09-22 14:35:02 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -20,7 +20,7 @@ export const config = {
   // 功能开关,打开：true，关闭：false
   SWITCH: {
     /** 每日天气 */
-    weather: true,
+    weather: false,
 
     /** 节假日 */
     // 下一休息日综合提醒
@@ -41,7 +41,7 @@ export const config = {
     poetry: false,
 
     /** 星座运势 */
-    horoscope: true,
+    horoscope: false,
 
     /** 生日消息和节日消息 */
     birthdayMessage: true,
@@ -62,9 +62,9 @@ export const config = {
       // 想要发送的人的名字
       name: "公主",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "o08wm6jut0BhaMY9A4IM4Vkt3EkI",
+      id: "o08wm6ilidQyZWM9FoQokWK0tP3k",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "EsMYWXWLUPD4UFwfSDk7G5NtMB38XFQvZfd3fQi-doc",
+      useTemplateId: "IorykqeWqdLrfckqJK5NjqVXm5nWd8AGoEKcoA1aSBg",
       // 所在省份
       province: "四川",
       // 所在城市
@@ -74,7 +74,7 @@ export const config = {
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://wangxinleo.cn",
+      openUrl: "https://s.weibo.com/top/summary?cate=realtimehot",
       // 专属节日提醒，如果你在这里填写了节日提醒，就不会执行FESTIVALS的提醒了, 和FESTIVALS的配置方法相同，可以往下查看，我这里就不重复写了
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
@@ -89,7 +89,7 @@ export const config = {
         // 在一起的日子
         {"keyword": "love_day", date: "2022-01-19"},
         // 结婚纪念日
-        {"keyword": "marry_day", date: "2022-09-09"},
+        {"keyword": "marry_day", date: "2022-09-16"},
         // 退伍日
         {"keyword": "ex_day", date: "2022-09-10"},
       ]
@@ -211,7 +211,7 @@ export const config = {
     // 在一起的日子
     {"keyword": "love_day", date: "2022-01-19"},
     // 结婚纪念日
-    {"keyword": "marry_day", date: "2022-09-09"},
+    {"keyword": "marry_day", date: "2022-09-16"},
     // 退伍日
     {"keyword": "ex_day", date: "2022-09-10"},
     // 你可以不断按格式往下增加
